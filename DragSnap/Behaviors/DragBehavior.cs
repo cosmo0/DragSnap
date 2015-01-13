9e447b8881e8cf9b1c620f345f5eb96e859191fa
Thomas Broust <thomas.broust@gmail.com>
2015-01-13 12:26:28 +0100

    Handles drag using custom behavior

---
+++ DragSnap/Behaviors/DragBehavior.cs
@@ -0,0 +1,94 @@
+﻿namespace DragSnap.Behaviors
+{
+    using System;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Media;
+
+    public class DragBehavior
+    {
+        public static readonly DependencyProperty IsDragProperty = DependencyProperty.RegisterAttached("Drag", typeof(bool), typeof(DragBehavior), new PropertyMetadata(false, OnDragChanged));
+
+        public readonly TranslateTransform Transform = new TranslateTransform();
+
+        private static DragBehavior _instance = new DragBehavior();
+
+        private Point _elementStartPosition2 = new Point(0, 0);
+
+        private Point _mouseStartPosition2 = new Point(0, 0);
+
+        public static DragBehavior Instance
+        {
+            get
+            {
+                return _instance;
+            }
+            set
+            {
+                _instance = value;
+            }
+        }
+
+        public static bool GetDrag(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsDragProperty);
+        }
+
+        public static void SetDrag(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsDragProperty, value);
+        }
+
+        private static void OnDragChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            // ignoring error checking
+            var element = (UIElement)sender;
+            var isDrag = (bool)(e.NewValue);
+
+            Instance = new DragBehavior();
+            ((UIElement)sender).RenderTransform = Instance.Transform;
+
+            if (isDrag)
+            {
+                element.MouseLeftButtonDown += Instance.ElementOnMouseLeftButtonDown;
+                element.MouseLeftButtonUp += Instance.ElementOnMouseLeftButtonUp;
+                element.MouseMove += Instance.ElementOnMouseMove;
+            }
+            else
+            {
+                element.MouseLeftButtonDown -= Instance.ElementOnMouseLeftButtonDown;
+                element.MouseLeftButtonUp -= Instance.ElementOnMouseLeftButtonUp;
+                element.MouseMove -= Instance.ElementOnMouseMove;
+            }
+        }
+
+        private void ElementOnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            var parent = Application.Current.MainWindow;
+            this._mouseStartPosition2 = mouseButtonEventArgs.GetPosition(parent);
+            ((UIElement)sender).CaptureMouse();
+        }
+
+        private void ElementOnMouseLeftButtonUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            ((UIElement)sender).ReleaseMouseCapture();
+            this._elementStartPosition2.X = this.Transform.X;
+            this._elementStartPosition2.Y = this.Transform.Y;
+        }
+
+        private void ElementOnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            var parent = Application.Current.MainWindow;
+            var mousePos = mouseEventArgs.GetPosition(parent);
+            var diff = (mousePos - this._mouseStartPosition2);
+            if (!((UIElement)sender).IsMouseCaptured)
+            {
+                return;
+            }
+
+            this.Transform.X = this._elementStartPosition2.X + diff.X;
+            this.Transform.Y = this._elementStartPosition2.Y + diff.Y;
+        }
+    }
+}
